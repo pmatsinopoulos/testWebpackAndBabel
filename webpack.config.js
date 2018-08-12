331a5bdd1bdadcb3b3bcacc9dca99bb14d1577b8
Panayotis Matsinopoulos <>
2018-08-12 17:59:20 +0300

    added module rules for babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
+      }
+    ]
   }
 };
 
