6f1590b0b860557bfdc5f66f6432c7ee905502a1
Panayotis Matsinopoulos <>
2018-08-12 18:12:13 +0300

    scr/js/index.js now has the correct JavaScript code to update the DOM in the index.html file.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import leftPad from './leftpad';
 
 
-const serNos = [6934, 23111, 23114, 1001, 211161];
-const strSNos = serNos.map(sn => leftPad(sn, 8, '0'));
-console.log(strSNos);
+const serNos = [123, 6934, 23111, 23114, 1001, 211161, 223];
+const partEl = document.getElementById('part-list');
+const strList = serNos.reduce(
+   (acc, element) => acc += `<li>${leftPad(element, 8, '0')}</li>`, ''
+);
+
+partEl.innerHTML = strList;
