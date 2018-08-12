81c746eebd557c2d8fa38ec4c20c8098a688371e
Panayotis Matsinopoulos <>
2018-08-12 18:04:49 +0300

    Corrected the path to bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "public/js"),
     filename: "bundle.js"
   },
   module: {
