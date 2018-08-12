400376d8756686084be079852902184c18de70ec
Panayotis Matsinopoulos <>
2018-08-12 18:29:23 +0300

    We webpack dev server

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import leftPad from './leftpad';
 
 
-const serNos = [123, 6934, 23111, 23114, 1001, 211161, 223];
+const serNos = [8, 111, 123, 6934, 23111, 23114, 1001, 211161, 223];
 const partEl = document.getElementById('part-list');
 const strList = serNos.reduce(
    (acc, element) => acc += `<li>${leftPad(element, 8, '0')}</li>`, ''
